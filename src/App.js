b0f396966da63045923f42db7b9df7fabec086f7
quonn <qber83@gmail.com>
2019-05-24 10:25:19 -0700

    Added Adoption and Landing Page Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import "./App.css"
 import Navigation from './Components/Navigation/Navigation'
+import AdoptionPage from './Components/AdoptionPage/AdoptionPage'
+import LandingPage from './Components/LandingPage/LandingPage'
+import { Route, Switch } from "react-router-dom";
+
 function App() {
+
+  const handleButtonClick = () => {
+    console.log("u")
+  }
+
   return (
     <div className="App">
       <header>
         <Navigation></Navigation>
       </header>
-
-      <section className="landing-banner">
-        <h1>Adopt A pet Today!</h1>
-      </section>
-      <main>
-        <p>As a pet lover, I want to go to the FIFO pet adoption
-           site so that I can get more information about the
-           adoption process
-  
-           Acceptance criteria
-  
-           When I go to the FIFO adoption agency site
-  
-           The site has a description of the adoption process
-           It has a meaningful picture that matches its description
-         A button indicating that I can start the adoption process</p>
-      </main>
-
+      <Switch>
+      <Route exact path={"/"}  component={LandingPage}/>
+      <Route exact path={"/adopt"} component={AdoptionPage} />
+      </Switch>
     </div>
   );
 }
