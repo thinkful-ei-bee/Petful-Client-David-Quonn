c7b05a106ad124d9e2a1c51a1d570115c8e917c8
David Bolin <david@bolin.at>
2019-05-24 12:50:37 -0700

    adding display for pets

--- src/Components/Pet/Pet.js
+++ src/Components/Pet/Pet.js
@@ -6,7 +6,15 @@ export default class Pet extends React.Component{
 
     render (){
         return (
-            <div>{this.props.pet.name}</div>
+            <>
+                <img src={this.props.pet.imageURL} height="300" width="300" alt="pet for adoption" />
+                <p>{this.props.pet.imageDescription}</p>
+                <p>Name: {this.props.pet.name}</p>
+                <p>Sex: {this.props.pet.sex}</p>
+                <p>Age: {this.props.pet.age}</p>
+                <p>Breed: {this.props.pet.breed}</p>
+                <p>Story: {this.props.pet.story}</p>
+            </>
         )
     }
 }
